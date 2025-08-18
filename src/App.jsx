de7867445e4f528acfe5bbc7a46c1fafbd8043c0
kim0tae <kimyt0402@gmail.com>
2025-08-18 20:14:37 +0900

    [UPDATE] 음수 조건 추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -18,11 +21,15 @@ function App() {
       };
     });
   }
+
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
